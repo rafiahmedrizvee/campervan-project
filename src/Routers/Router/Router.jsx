29252564e3cdc183b0699c31ecc335e83d97b2f1
rafiahmedrizvee <rizvirafi7@gmail.com>
2026-03-07 01:08:15 +0600

    design complete

--- src/Routers/Router/Router.jsx
+++ src/Routers/Router/Router.jsx
@@ -2,17 +2,22 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home";
 import Shop from "../../Pages/Shop/Shop";
-import Brands from "../../Pages/Brands/Brands";
 import Blog from "../../Pages/Blog/Blog";
 import ContactUs from "../../Pages/ContactUs/ContactUs";
 import BlogDetails from "../../Pages/Blog/BlogDetails";
 import ProductDetails from "../../Pages/ProductDetails";
 import About from "../../Pages/About/About";
+import ScrollToTop from "../../components/ScrollToTop";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Main></Main>,
+     element: (
+      <>
+        <ScrollToTop />
+        <Main />
+      </>
+    ),
     children: [
         {
                 path:"/",
@@ -34,11 +39,7 @@ const router = createBrowserRouter([
                 element:<ProductDetails></ProductDetails>
 
         },
-        {
-                path:"/brands",
-                element:<Brands></Brands>
-
-        },
+       
         {
                 path:"/blog",
                 element:<Blog></Blog>
